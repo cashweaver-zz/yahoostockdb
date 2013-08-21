86a8553d63766a647808f80fe2ff59d48073ad51
DeusExVerbum <mymorningpromenade@gmail.com>
2013-08-20 22:16:14 -0700

    Still getting errors when running with multiple processes.

--- __init__.py
+++ __init__.py
@@ -18,10 +18,9 @@ import mpprogressbar
 
 
 config = configparser.ConfigParser()
-config.read(os.getcwd()+"/config.ini")
-symbol_list = config['DATABASE']['symbol_list']
+config.read(os.path.dirname(os.path.realpath(__file__))+"/config.ini")
+symbol_list = config['DATABASE']['symbol_list_path']
 db_path = config['DATABASE']['db_path']
-conn_test_ip = config['DATABASE']['conn_test_ip']
 
 # set up log
 log = logbook.Logger('Logbook')
@@ -33,6 +32,7 @@ last_week = now - datetime.timedelta(days=7)
 last_week = last_week.strftime("%Y-%m-%d")
 the_beginning = "1900-01-01"
 
+# TODO move these to config file
 # column names
 cols_hist = "Date TEXT, " + \
         "Open REAL, " + \
@@ -47,49 +47,54 @@ cols_ta = "Rsi14 REAL, " + \
 
 class Database(object):
     def __init__(self):
+        """
+        Desc
+
+        @type: number
+        @param: the_parameter
+
+        @rtype: number
+        @return: A_number
+        """
         pass
 
 
-    def mp_update_db(self, symbol_list=symbol_list, db_path=db_path):
+    def update_db(self):
+        """
+        Desc
+
+        @type: number
+        @param: the_parameter
+
+        @rtype: number
+        @return: A_number
+        """
+        """
+        Updates the database using multiprocessing, splitting all symbols
+        defined in the symbol list over [multiprocessing.cpu_count()] processes.
+        """
+
         log.info("Checking for internet connection...")
         if self.internet_on():
             log.info("Connection found. Continuing...")
             procs = []
             try:
                 log.info("Updating database...")
-                con = lite.connect(db_path)
+                con = lite.connect(config['DATABASE']['db_path'])
                 with con:
-
-                    # Configuration Options
-                    #======================
-                    # Update interval in seconds
-                    # Numbers below 1 may result in flickering
-                    #pb_update_interval = 1
-                    # Message to be printed once all processes finish
-                    #end_msg =  'All processes complete!\n'
-                    # Prefix for all process names. Be sure to include a trailing space for
-                    # legibility
-                    #pname_prefix = "Process "
-                    # Header to be included above progress bars
-                    #header = "Running processes"
-                    # Number of processes
+                    # Get the number of processors on our system
                     nprocs = mp.cpu_count()
+                    log.info("%d cores detected." % nprocs)
 
-
-                    # Create dummy list and data
-                    #===========================
-                    #nums = []
-                    #nums.extend(range(1,49))
-                    # Split nums, as equally as can be done, into a list of lists: sub_nums
-                    #sub_nums = [nums[i*len(nums)//nprocs:(i+1)*len(nums)//nprocs] for i in range(nprocs)]
-                    all_syms = [i.strip() for i in open(symbol_list, 'r').readlines()]
+                    # Pull all symbols from given symbol_list_path
+                    sym_path = config['DATABASE']['symbol_list_path']
+                    all_syms = [i.strip() for i in open(sym_path, 'r').readlines()]
+                    # Evenly distribute all symbols over nproc lists
                     sub_syms = [all_syms[i*len(all_syms)//nprocs:(i+1)*len(all_syms)//nprocs] for i in range(nprocs)]
+                    log.info("%d symbols found." % len(all_syms))
 
-
-                    # Create overseer lists and dicts
-                    #================================
                     # Dictionary to hold the current % of all processes,
-                    # indexed by pid
+                    #   indexed by pid
                     pstatus = mp.Manager().dict()
                     # Create list to hold all processes.
                     procs = []
@@ -98,9 +103,6 @@ class Database(object):
 
 
                     # Create the global progress bar
-                    #===============================
-                    # Define progress bar in the same way you would with the regular
-                    # progressbar package.
                     #   NOTE: maxval must equal the sum of maxvals for all processes
                     pbars.append(mpprogressbar.ProgressBar(
                         widgets=[
@@ -114,23 +116,16 @@ class Database(object):
                     # Initialize status of the global process to 0%
                     pstatus[0] = 0
 
-
-                    print "ABOUT TO ENTER PROC CREATING FOR LOOP"
                     # Create a progress bar for each process
-                    #=======================================
                     for i in range(nprocs):
-                        # Maximum value of the progress bar. Defined within the loop because
-                        # this number should be equal to the operation count of the given
-                        # process, which may not be equal for all processes.
-                        #pb_maxval = len(sub_syms[i])
-
                         # pid = i+1 because 0 is reserved for the global progress bar
                         pid = i + 1
                         # Initialize status of the current process to 0%
                         pstatus[pid] = 0
+                        # Create db connection
+                        #con = lite.connect(config['DATABASE']['db_path'])
 
-                        # Define progress bar in the same way you would with the regular
-                        # progressbar package
+                        # Define progress bar for each process
                         pbars.append(mpprogressbar.ProgressBar(
                             widgets=[
                                 ('Process ' + str(pid)), ' ',
@@ -141,70 +136,98 @@ class Database(object):
                             ).start())
 
 
+                        log.info("Creating process %d/%d" % (pid, nprocs))
                         # Initialize the new process
                         #   target: function to be run by the process
                         #   args: list of arguments the function requires
                         child = mp.Process(
-                            target=self.do_stuff,
+                            target=self.worker,
                             args=[sub_syms[i], pstatus, pid, con])
 
                         # Add child to procs so we can keep track of it
                         procs.append(child)
 
+                        log.info("Starting process %d/%d" % (pid, nprocs))
                         # Start the process
                         child.start()
 
-                    # Prints progress bars while any one is still alive, then prints the end_msg
+                    # Prints progress bars while any one is still alive
                     self.print_progressbars(
                         pstatus,
                         procs,
-                        pbars
-                        )
+                        pbars)
             except lite.Error, e:
                 log.critical("Error: %s: " % e.args[0])
                 log.critical("Closing all processses...")
                 for i, p in enumerate(procs):
                     p.join()
                     log.critical("Process %d closed." % i)
-
+                sys.exit(1)
+            except KeyboardInterrupt:
+                log.critical("Database update interupted by keyboard")
+                log.critical("Closing all processses...")
+                for i, p in enumerate(procs):
+                    p.join()
+                    log.critical("Process %d closed." % i)
+                log.warning("Your database may have become corrupted.")
                 sys.exit(1)
             finally:
-                pass
-                #if con:
-                    #con.close()
+                log.warning("Open connection found. Closing...")
+                if con:
+                    con.close()
         else:
-            log.critical("Could not connect to google.com via [%s]. Conclusion:  You're not connected to the internet. Either that or google.com is down. 2013-08-17 Never Forget." % conn_test_ip)
-            pass
+            log.critical("Could not connect to google.com via [%s]. \
+                         Conclusion:  You're not connected to the internet. \
+                         Either that or google.com is down. \
+                         2013-08-17 Never Forget." % config['DATABASE']['conn_test_ip'])
 
-    def do_stuff(self, sub_syms, pstatus, pid, con):
+    def worker(self, sub_syms, pstatus, pid, con):
         """
-        Pretends to do work so we can see mpprogressbar in action.
+        Desc
+
+        @type: number
+        @param: the_parameter
+
+        @rtype: number
+        @return: A_number
+        """
+        """
+        Each process invoked by update_db() is set to this function.
         """
         for s in sub_syms:
             self.update_symbol(con, s)
             # update progress bar for this process
-            #print "proc_" + str(pid) + ": updated!"
             pstatus[pid] += 1
             # update the global progress bar
             pstatus[0] += 1
 
     def print_progressbars(self, pstatus, procs, pbars):
         """
-        Print all progress bars while any one is still running. Then print the
-        end_msg
+        Desc
+
+        @type: number
+        @param: the_parameter
+
+        @rtype: number
+        @return: A_number
+        """
+        """
+        Print all progress bars while any one is still running.
         """
         header_msg = config['DATABASE']['pb_header']
         pb_refresh_interval = float(config['DATABASE']['pb_refresh_interval'])
         # Immediately clear the screen and print our header.
         # Without this, if the pb_update_interval is relatively long, the script
         # will appear to 'hang', even though it is running properly.
-        sys.stderr.write('\033[2J\033[H') # clear screen
+        #sys.stderr.write('\033[2J\033[H') # clear screen
         print header_msg
 
         # Continually update all progress bars so long as any one is still alive
         while any(p.is_alive() for p in procs):
             time.sleep(pb_refresh_interval)
-            sys.stderr.write('\033[2J\033[H') # clear screen
+            #sys.stderr.write('\033[2J\033[H') # clear screen
+            #sys.stderr.write('\033[2J\033[H') # clear screen
+            sys.stderr.write('\n');
             print header_msg
             # progressbar handles the actual printing of progress bars
             for i, pbar in enumerate(pbars):
@@ -213,43 +236,16 @@ class Database(object):
         print config['DATABASE']['pb_complete_msg']
 
 
-    #def update_db(self, symbol_list=symbol_list, db_path=db_path):
-        #log.info("Checking for internet connection...")
-        #if self.internet_on():
-            #log.info("Connection found. Continuing...")
-            #try:
-                ##log.info("Updating database...")
-                #con = lite.connect(":memory:")
-                #with con:
-                    #symbol_count = self.file_len(symbol_list)
-                    #pbar = progressbar.ProgressBar(
-                        #widgets=['Updating database: ',
-                                #progressbar.Bar('=', '[', ']'), ' ',
-                                #progressbar.Percentage(), ' ',
-                                #progressbar.ETA()],
-                        #maxval = symbol_count).start()
-                    #cur_sym = 0
-                    #i = 0
-                    #for symbol in open(symbol_list, 'r').readlines():
-                        #pbar.update(i+1)
-                        #i += 1
-                        ## sterilize symbol: change all special chars into '_'
-                        #symbol = self.sterilize_symbol(symbol)
-                        #self.update_symbol(con, symbol, db_path)
-                        #cur_sym += 1
-            #except lite.Error, e:
-                #log.critical("Error: %s: " % e.args[0])
-                #sys.exit(1)
-            #finally:
-                #if con:
-                    #con.close()
-        #else:
-            #log.critical("Could not connect to google.com via [%s]. Conclusion:  You're not connected to the internet. Either that or google.com is down. 2013-08-17 Never Forget." % conn_test_ip)
-            #pass
-
-
     def update_symbol(self, con, symbol):
-        #db_path = config['DATABASE']['db_path']
+        """
+        Desc
+
+        @type: number
+        @param: the_parameter
+
+        @rtype: number
+        @return: A_number
+        """
         lrd = self.get_latest_remote_date(symbol)
         # check that Yahoo has data for given symbol
         if not lrd == "":
@@ -277,6 +273,15 @@ class Database(object):
 
 
     def get_latest_local_date(self, con, symbol):
+        """
+        Desc
+
+        @type: number
+        @param: the_parameter
+
+        @rtype: number
+        @return: A_number
+        """
         symbol = symbol.upper()
         # ensure connection to db
         cur = con.cursor()
@@ -285,6 +290,15 @@ class Database(object):
 
 
     def get_latest_remote_date(self, symbol):
+        """
+        Desc
+
+        @type: number
+        @param: the_parameter
+
+        @rtype: number
+        @return: A_number
+        """
         # assumes that the most recent remote date will be in the last week
         try:
             hist_data = ysq.get_historical_prices(symbol, last_week, today)
@@ -295,6 +309,15 @@ class Database(object):
 
 
     def init_tables(self, con, symbol):
+        """
+        Desc
+
+        @type: number
+        @param: the_parameter
+
+        @rtype: number
+        @return: A_number
+        """
         symbol = self.sterilize_symbol(symbol)
         cur = con.cursor()
         cur.execute("DROP TABLE IF EXISTS %s_HIST" % symbol)
@@ -305,13 +328,36 @@ class Database(object):
 
 
     def table_exists(self, con, t_name):
-        """Check if table exists. Returns true/false"""
+        """
+        Desc
+
+        @type: number
+        @param: the_parameter
+
+        @rtype: number
+        @return: A_number
+        """
+        """
+        Check if table exists.
+
+        @rtype: bool
+        @return: True, if the table exists, else false.
+        """
         cur = con.cursor()
         cur.execute("SELECT 1 FROM sqlite_master WHERE type='table' AND name='%s'" % (t_name))
         return cur.fetchone()
 
 
     def update_tables(self, con, symbol, sdate, edate):
+        """
+        Desc
+
+        @type: number
+        @param: the_parameter
+
+        @rtype: number
+        @return: A_number
+        """
         update_count = 0
         cur = con.cursor()
         hist_data = ysq.get_historical_prices(symbol, sdate, edate)
@@ -336,18 +382,45 @@ class Database(object):
 
 
     def sterilize_symbol(self, symbol):
+        """
+        Desc
+
+        @type: number
+        @param: the_parameter
+
+        @rtype: number
+        @return: A_number
+        """
         return re.sub('[^a-zA-Z0-9]', '_', symbol.rstrip().upper())
 
 
     def internet_on(self):
+        """
+        Desc
+
+        @type: number
+        @param: the_parameter
+
+        @rtype: number
+        @return: A_number
+        """
         try:
-            urllib2.urlopen('http://'+conn_test_ip, timeout=1)
+            urllib2.urlopen('http://'+config['DATABASE']['conn_test_ip'], timeout=1)
             return True
         except urllib2.URLError: pass
         return False
 
 
     def file_len(self, fname):
+        """
+        Returns the length of a file
+
+        @type: string
+        @param: Name of the file to be checked.
+
+        @rtype: number
+        @return: A_number
+        """
         with open(fname) as f:
             for i, l in enumerate(f):
                 pass
