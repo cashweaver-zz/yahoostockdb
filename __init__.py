5c6e86e3d3bb95c9f0f5232bc54c255874f51f7a
DeusExVerbum <mymorningpromenade@gmail.com>
2013-08-22 22:11:54 -0700

    Broken, committing for backup

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ import time
 from sqlite3 import dbapi2 as lite
 import multiprocessing as mp
 import ystockquote as ysq
-import mpprogressbar
+import progressbar
 
 
 config = configparser.ConfigParser()
@@ -86,76 +86,193 @@ class Database(object):
                     nprocs = mp.cpu_count()
                     log.info("%d cores detected." % nprocs)
 
-                    # Pull all symbols from given symbol_list_path
-                    sym_path = config['DATABASE']['symbol_list_path']
-                    all_syms = [i.strip() for i in open(sym_path, 'r').readlines()]
-                    # Evenly distribute all symbols over nproc lists
-                    sub_syms = [all_syms[i*len(all_syms)//nprocs:(i+1)*len(all_syms)//nprocs] for i in range(nprocs)]
-                    log.info("%d symbols found." % len(all_syms))
+                    nprocs = 16
 
-                    # Dictionary to hold the current % of all processes,
-                    #   indexed by pid
+                    all_syms = [i.strip() for i in open(symbol_list, 'r').readlines()]
+                    sub_syms = [all_syms[i*len(all_syms)//nprocs:(i+1)*len(all_syms)//nprocs] for i in range(nprocs)]
+                    dead_symbols = []
+
+                    hist_data_q = mp.Queue()
+                    producers = []
                     pstatus = mp.Manager().dict()
                     # Create list to hold all processes.
-                    procs = []
                     # Create list to hold all progress bars.
-                    pbars = []
-
+                    #pbars = []
 
                     # Create the global progress bar
                     #   NOTE: maxval must equal the sum of maxvals for all processes
-                    pbars.append(mpprogressbar.ProgressBar(
+                    pbar = progressbar.ProgressBar(
                         widgets=[
                             'All Procs', ' ',
-                            mpprogressbar.Percentage(), ' ',
-                            mpprogressbar.Bar('#', '[', ']'), ' ',
-                            mpprogressbar.ETA()],
+                            progressbar.Percentage(), ' ',
+                            progressbar.Bar('#', '[', ']'), ' ',
+                            progressbar.ETA()],
                             maxval=len(all_syms)
-                        ).start())
+                        ).start()
 
                     # Initialize status of the global process to 0%
                     pstatus[0] = 0
 
-                    # Create a progress bar for each process
                     for i in range(nprocs):
                         # pid = i+1 because 0 is reserved for the global progress bar
                         pid = i + 1
                         # Initialize status of the current process to 0%
-                        pstatus[pid] = 0
+                        #pstatus[pid] = 0
+
+                        # Define progress bar for each process
+                        #pbars.append(mpprogressbar.ProgressBar(
+                            #widgets=[
+                                #('Process %02d' % pid), ' ',
+                                #mpprogressbar.Percentage(), ' ',
+                                #mpprogressbar.Bar('-', '[', ']'), ' ',
+                                #mpprogressbar.ETA()],
+                                #maxval=len(sub_syms[i])
+                            #).start())
+
+                        log.info("Creating process %d/%d" % (pid, nprocs))
+                        child = mp.Process(
+                            target=self._producer,
+                            args=[sub_syms[i], hist_data_q, pstatus, pid])
+                        producers.append(child)
+                        child.start()
+
+                    #log.info("--------------------------------------")
+                    #self.print_progressbars(
+                        #pstatus,
+                        #producers,
+                        #pbars)
+
+                    # TODO: this needs to be re-worked.
+                    # Currently, the whole table is downloaded regardless
+                    # of the next action being an update, initialization, or drop.
+                    # It is a waste of bandwidth.
+
+
+                    cursor = con.cursor()
+                    #log.info("--------------------------------------")
+                    while any(p.is_alive() for p in producers):
+                        symbol, lrd, hist_data = hist_data_q.get()
+                        symbol = self.sterilize_symbol(symbol)
+
+                        # if table (symbol)_HIST exists, this is an update
+                        if table_exists(cursor, symbol):
+                            lld = get_latest_local_date(cursor, symbol)
+                            if lld == lrd:
+                                log.info("Already up-to-date: %s" % (symbol))
+                            else:
+                                lld = time.strptime(lld, "%Y-%m-%d")
+                                lrd = time.strptime(lld, "%Y-%m-%d")
+                                # Don't download data for symbols that have
+                                # not been updated since last_week.
+                                if (lrd > last_week) and (lrd > llr):
+                                    log.info("Updating: %s" % (symbol))
+                                    self._insert_tables(cursor, symbol, hist_data, 'placeholder')
+                                # If the symbol hasn't been updated since
+                                # last week, drop our tables and flag it.
+                                else:
+                                    self._drop_tables(cursor, symbol)
+                                    dead_symbols.append(symbol)
+                        # otherwise, it's an initialization
+                        else:
+                            self._drop_tables(cursor, symbol)
+                            self._create_tables(cursor, symbol)
+                            #self._insert_tables(cursor, symbol, hist_data, ta_data)
+                            self._insert_tables(cursor, symbol, hist_data, 'placeholder')
+
+
+
+
+
+
+                        log.info("Creating table for %s" % (symbol))
+                        cursor.execute("DROP TABLE IF EXISTS %s_HIST" % (symbol))
+                        cursor.execute("CREATE TABLE %s_HIST(%s)" % (symbol, cols_hist))
+                        cursor.executemany("INSERT INTO %s_HIST VALUES(?, ?, ?, ?, ?, ?, ?)" % (symbol), (hist_data))
+
+                        pbar.update(pstatus[0])
+
+                        # if table (symbol)_HIST exists, this is an update
+                            # if lrd == lld, do nothing
+                            # else
+                            # else lrd > lld, update
+                            # if lrd < last_week, drop the table, add symbol to list of badness
+
+                        # else this is an initilization
+                            # if lrd < last_week, drop the table, add symbol to list of badness
+                            # else, drop tables if they exist, create new tables, insert data
+
+
+
+                    # Pull all symbols from given symbol_list_path
+                    #sym_path = config['DATABASE']['symbol_list_path']
+                    #all_syms = [i.strip() for i in open(sym_path, 'r').readlines()]
+                    # Evenly distribute all symbols over nproc lists
+                    #sub_syms = [all_syms[i*len(all_syms)//nprocs:(i+1)*len(all_syms)//nprocs] for i in range(nprocs)]
+                    #log.info("%d symbols found." % len(all_syms))
+
+                    # Dictionary to hold the current % of all processes,
+                    #   indexed by pid
+                    #pstatus = mp.Manager().dict()
+                    # Create list to hold all processes.
+                    #procs = []
+                    # Create list to hold all progress bars.
+                    #pbars = []
+
+
+                    # Create the global progress bar
+                    #   NOTE: maxval must equal the sum of maxvals for all processes
+                    #pbars.append(mpprogressbar.ProgressBar(
+                        #widgets=[
+                            #'All Procs', ' ',
+                            #mpprogressbar.Percentage(), ' ',
+                            #mpprogressbar.Bar('#', '[', ']'), ' ',
+                            #mpprogressbar.ETA()],
+                            #maxval=len(all_syms)
+                        #).start())
+
+                    # Initialize status of the global process to 0%
+                    #pstatus[0] = 0
+
+                    # Create a progress bar for each process
+                    #for i in range(nprocs):
+                        # pid = i+1 because 0 is reserved for the global progress bar
+                        #pid = i + 1
+                        # Initialize status of the current process to 0%
+                        #pstatus[pid] = 0
                         # Create db connection
                         #con = lite.connect(config['DATABASE']['db_path'])
 
                         # Define progress bar for each process
-                        pbars.append(mpprogressbar.ProgressBar(
-                            widgets=[
-                                ('Process ' + str(pid)), ' ',
-                                mpprogressbar.Percentage(), ' ',
-                                mpprogressbar.Bar('-', '[', ']'), ' ',
-                                mpprogressbar.ETA()],
-                                maxval=len(sub_syms[i])
-                            ).start())
+                        #pbars.append(mpprogressbar.ProgressBar(
+                            #widgets=[
+                                #('Process ' + str(pid)), ' ',
+                                #mpprogressbar.Percentage(), ' ',
+                                #mpprogressbar.Bar('-', '[', ']'), ' ',
+                                #mpprogressbar.ETA()],
+                                #maxval=len(sub_syms[i])
+                            #).start())
 
 
-                        log.info("Creating process %d/%d" % (pid, nprocs))
+                        #log.info("Creating process %d/%d" % (pid, nprocs))
                         # Initialize the new process
                         #   target: function to be run by the process
                         #   args: list of arguments the function requires
-                        child = mp.Process(
-                            target=self.worker,
-                            args=[sub_syms[i], pstatus, pid, con])
+                        #child = mp.Process(
+                            #target=self.worker,
+                            #args=[sub_syms[i], pstatus, pid, con])
 
                         # Add child to procs so we can keep track of it
-                        procs.append(child)
+                        #procs.append(child)
 
-                        log.info("Starting process %d/%d" % (pid, nprocs))
+                        #log.info("Starting process %d/%d" % (pid, nprocs))
                         # Start the process
-                        child.start()
+                        #child.start()
 
                     # Prints progress bars while any one is still alive
-                    self.print_progressbars(
-                        pstatus,
-                        procs,
-                        pbars)
+                    #self.print_progressbars(
+                        #pstatus,
+                        #procs,
+                        #pbars)
             except lite.Error, e:
                 log.critical("Error: %s: " % e.args[0])
                 log.critical("Closing all processses...")
@@ -171,35 +288,41 @@ class Database(object):
                     log.critical("Process %d closed." % i)
                 log.warning("Your database may have become corrupted.")
                 sys.exit(1)
-            finally:
-                log.warning("Open connection found. Closing...")
-                if con:
-                    con.close()
         else:
             log.critical("Could not connect to google.com via [%s]. \
                          Conclusion:  You're not connected to the internet. \
                          Either that or google.com is down. \
                          2013-08-17 Never Forget." % config['DATABASE']['conn_test_ip'])
 
-    def worker(self, sub_syms, pstatus, pid, con):
-        """
-        Desc
-
-        @type: number
-        @param: the_parameter
-
-        @rtype: number
-        @return: A_number
-        """
-        """
-        Each process invoked by update_db() is set to this function.
-        """
+    def _producer(self, sub_syms, q, pstatus, pid):
+        log.info("Process %02d starting" % (pid))
         for s in sub_syms:
-            self.update_symbol(con, s)
-            # update progress bar for this process
-            pstatus[pid] += 1
-            # update the global progress bar
-            pstatus[0] += 1
+            try:
+                hist_data = ysq.get_historical_prices(s, the_beginning, today)
+                # remove header
+                hist_data.pop(0)
+                lrd = hist_data[0][0]
+                # reverse order, so oldest are at top
+                hist_data.reverse()
+                q.put((s, lrd, hist_data))
+                pstatus[0] += 1
+            except:
+                log.info("Process %02d exception!" % (pid))
+                pass
+        log.info("Process %02d finished" % (pid))
+
+    def _drop_tables(self, cursor, symbol):
+        cursor.execute("DROP TABLE IF EXISTS %s_HIST" % (symbol))
+        cursor.execute("DROP TABLE IF EXISTS %s_TA" % (symbol))
+
+    # assumes tables don't exist
+    def _create_tables(self, cursor, symbol):
+        cursor.execute("CREATE TABLE %s_HIST" % (symbol))
+        cursor.execute("CREATE TABLE %s_TA" % (symbol))
+
+    def _insert_tables(self, cursor, symbol, hist_data, ta_data):
+        cursor.executemany("INSERT INTO %s_HIST VALUES(?, ?, ?, ?, ?, ?, ?)" % (symbol), (hist_data))
+        #cursor.executemany("INSERT INTO %s_TA VALUES(?, ?)" % (symbol), (ta_data))
 
     def print_progressbars(self, pstatus, procs, pbars):
         """
@@ -219,15 +342,13 @@ class Database(object):
         # Immediately clear the screen and print our header.
         # Without this, if the pb_update_interval is relatively long, the script
         # will appear to 'hang', even though it is running properly.
-        #sys.stderr.write('\033[2J\033[H') # clear screen
+        sys.stderr.write('\033[2J\033[H') # clear screen
         print header_msg
 
         # Continually update all progress bars so long as any one is still alive
         while any(p.is_alive() for p in procs):
             time.sleep(pb_refresh_interval)
-            #sys.stderr.write('\033[2J\033[H') # clear screen
-            #sys.stderr.write('\033[2J\033[H') # clear screen
-            sys.stderr.write('\n');
+            sys.stderr.write('\033[2J\033[H') # clear screen
             print header_msg
             # progressbar handles the actual printing of progress bars
             for i, pbar in enumerate(pbars):
@@ -272,7 +393,7 @@ class Database(object):
             pass
 
 
-    def get_latest_local_date(self, con, symbol):
+    def get_latest_local_date(self, cursor, symbol):
         """
         Desc
 
@@ -284,9 +405,8 @@ class Database(object):
         """
         symbol = symbol.upper()
         # ensure connection to db
-        cur = con.cursor()
-        cur.execute("SELECT * FROM %s_HIST WHERE oid = (SELECT MAX(oid) FROM %s_HIST)" % (symbol, symbol))
-        return cur.fetchone()[0]
+        cursor.execute("SELECT * FROM %s_HIST WHERE oid = (SELECT MAX(oid) FROM %s_HIST)" % (symbol, symbol))
+        return cursor.fetchone()[0]
 
 
     def get_latest_remote_date(self, symbol):
@@ -327,7 +447,7 @@ class Database(object):
         con.commit()
 
 
-    def table_exists(self, con, t_name):
+    def table_exists(self, cursor, t_name):
         """
         Desc
 
@@ -343,9 +463,8 @@ class Database(object):
         @rtype: bool
         @return: True, if the table exists, else false.
         """
-        cur = con.cursor()
-        cur.execute("SELECT 1 FROM sqlite_master WHERE type='table' AND name='%s'" % (t_name))
-        return cur.fetchone()
+        cursor.execute("SELECT 1 FROM sqlite_master WHERE type='table' AND name='%s'" % (t_name))
+        return cursor.fetchone()
 
 
     def update_tables(self, con, symbol, sdate, edate):
